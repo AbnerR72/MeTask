4081aba59c6f6c2a3a0893046f9df4fe070720f0
Abner Ruiz Santiago <abner_ruiz12@comunidad.unam.mx>
2025-04-27 13:44:22 -0600

    Implementacion de la navegacion

--- app/src/main/java/com/example/metask/LoginFragment.kt
+++ app/src/main/java/com/example/metask/LoginFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.metask.databinding.FragmentSecondBinding
+import com.example.metask.databinding.FragmentLoginFragmentBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class LoginFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentLoginFragmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentLoginFragmentBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +32,8 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.button.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.textView.setOnClickListener { // Cambia button2 por textViewNavigate
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
     }
 
