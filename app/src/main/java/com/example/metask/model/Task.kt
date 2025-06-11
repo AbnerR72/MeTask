b8416ba72da9af4d5fa41b9dfa3bbf7c1096cedb
Abner Ruiz Santiago <abner_ruiz12@comunidad.unam.mx>
2025-06-11 14:26:31 -0600

    Implementacion del modelo para las tareas

---
+++ app/src/main/java/com/example/metask/model/Task.kt
@@ -0,0 +1,11 @@
+package com.example.metask.model
+
+import java.util.Date
+
+data class Task (
+    val id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val bornDate: Date = Date(),
+    val isCompleted: Boolean = false  // Útil para filtrar pendientes/completadas
+)
